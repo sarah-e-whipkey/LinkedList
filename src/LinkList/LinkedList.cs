f7698f24a5872c66ec0e38f958c4141de81e9f06
Sarah Whipkey <sarah.e.whipkey@gmail.com>
2016-03-25 11:55:36 -0500

    linked list how to

---
+++ src/LinkList/LinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    public class LinkedList<LLT>
+    {
+        private Node<LLT> Head { get; set; }
+        private Node<LLT> Tail { get; set; }
+
+        public void Add(LLT ele)
+        {
+            var newNode = new Node<LLT>()
+            {
+                Value = ele
+            };
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+        }
+
+        public LLT Lookup(int index) {
+
+            var currentNode = Head;
+
+            for (int i = 0; i < index && currentNode != null; i++) {
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode == null || index < 0) {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+
+            return currentNode.Value;
+        }
+
+        public void Delete(LLT ele) {
+
+            var currentNode = Head;
+
+            if (currentNode != null && ele.Equals(currentNode.Value)) {
+                Head = Head.Next;
+                return;
+            }
+
+            while (currentNode?.Next != null &&
+                !currentNode.Next.Value.Equals(ele)) {
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode?.Next != null) {
+                
+                currentNode.Next = currentNode.Next.Next;
+
+                if (currentNode.Next == null) {
+                    Tail = currentNode;
+                }
+            }
+        }
+    }
+
+     class Node<T> {
+
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ src/LinkList/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LinkList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinkList")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b25e4f37-efec-4e52-97d3-36eaf213e703")]
